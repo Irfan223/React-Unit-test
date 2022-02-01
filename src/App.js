258cd25577c36460c818ca7efe213f74e72c4f92
Md Irfan <mohammad_irfan@epam.com>
2022-02-01 12:53:14 +0530

    configure redux and proptypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './component/header/index';
 import Headline from './component/headline';
 import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <section className="main">
-        <Headline header="posts" desc="Click the button to render posts!" />
-      </section>
 
-    </div>
-  );
+const tempArr = [{
+  fName: "Md",
+  lName: "Irfan",
+  email: "mdirfan844@gmail.com",
+  age: 27,
+  onlineStatus: true
+}]
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <section className="main">
+          <Headline
+            header="posts"
+            desc="Click the button to render posts!"
+            tempArr={tempArr}
+          />
+        </section>
+
+      </div>
+    );
+  }
 }
 
 export default App;
